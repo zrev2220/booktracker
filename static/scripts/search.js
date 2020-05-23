4671cd8e22d16b3bff6ea49b8b232349fc1250d3
Zac Hayes <zrev2220@gmail.com>
2020-05-23 16:56:06 -0400

    Fix bug: filter button style out of sync with collapse divs
    This issue was caused by rapid button clicks, where the button's class
    would get toggled before the collapse animation completed.
    
    This commit fixes the issue by storing the divs in the process of
    collapsing, ignoring button clicks targeting those divs until their
    animations complete.
    
    Fixes #8.

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,10 +1,28 @@
-$().ready(() => {
+"use strict";
+
+/**
+ * Set of currently-displayed filters.
+ * @type {Set<string>}
+ */
+let filters;
+/**
+ * Set of div elements which have triggered a collapse process that has not finished yet.
+ *
+ * This is used to prevent rapid clicks causing a button's appearance to become inconsistent with its div's collapse
+ * state.
+ * @type {Set<HTMLDivElement>}
+ */
+const collapsingFilters = new Set();
+
+$(() => {
+  // register event handlers
   $("#searchForm").on("submit", e => onFormSubmit(e));
-  let $filterBtns = $(".filter-btn").on("click", e => toggleFilter(e.target));
+  const $filterBtns = $(".filter-btn").on("click", e => toggleFilter(e.target));
+  $(".collapse").on("shown.bs.collapse hidden.bs.collapse", e => collapsingFilters.delete(e.target));
 
   // make sure active filters' buttons are initialized
-  let filters = new Set(JSON.parse($("#activeFilters").val()));
-  for (let btn of Array.from($filterBtns).filter(b => filters.has($(b).data("filter")))) {
+  filters = new Set(JSON.parse($("#activeFilters").val()));
+  for (const btn of Array.from($filterBtns).filter(b => filters.has($(b).data("filter")))) {
     toggleFilter(btn, true);
     $(btn.dataset.target).collapse("show");
   }
@@ -18,8 +36,8 @@ function onFormSubmit(e) {
   e.preventDefault();
   console.log(e);
 
-  let $form = $("#searchForm");
-  let data = {};
+  const $form = $("#searchForm");
+  const data = {};
   $form.serializeArray().forEach(obj => data[obj.name] = obj.value);
   $.post({
     url: "/search",
@@ -31,21 +49,23 @@ function onFormSubmit(e) {
 }
 
 function displayResults(data) {
-  let $table = $("#resultsTable");
-  let $noResults = $("#noResultsLabel");
+  const $table = $("#resultsTable");
+  const $noResults = $("#noResultsLabel");
   if (data.items.length > 0) {
     $table.removeClass("d-none");
     $noResults.addClass("d-none");
-    let $tbody = $("#resultsBody").html("");
-    for (let book of data.items) {
-      let nameParts = [];
+    const $tbody = $("#resultsBody").html("");
+    for (const book of data.items) {
+      const nameParts = [];
       if (book.author_last) nameParts.push(book.author_last);
       if (book.author_first) nameParts.push(book.author_first);
-      let name = nameParts.join(", ");
-      let row = $("<tr></tr>").toggleClass("checked-out", book.checkout);
-      row.append(`<td>${name}</td>`,
-                 `<td>${book.title}</td>`,
-                 `<td class="d-none d-sm-table-cell">${book.location}</td>`);
+      const name = nameParts.join(", ");
+      const row = $("<tr></tr>").toggleClass("checked-out", book.checkout);
+      row.append(
+        `<td>${name}</td>`,
+        `<td>${book.title}</td>`,
+        `<td class="d-none d-sm-table-cell">${book.location}</td>`
+      );
       $tbody.append(row);
     }
   } else {
@@ -54,26 +74,32 @@ function displayResults(data) {
   }
 }
 
-function toggleFilter(btn, init=false) {
-  let thisFilter = btn.dataset.filter;
-  let $filterInput = $("#activeFilters");
-  btn.classList.toggle("btn-outline-primary");
-  btn.classList.toggle("btn-primary");
-  let filters = new Set(JSON.parse($filterInput.val()));
-  if (!init) {
-    if (filters.has(thisFilter))
-      filters.delete(thisFilter);
-    else
-      filters.add(thisFilter);
+function toggleFilter(btn, init = false) {
+  const thisFilter = btn.dataset.filter;
+  const $filterElement = $(btn.dataset.target);
+  if (!collapsingFilters.has($filterElement[0]) || init) {
+    if (!init) {
+      // if not init'ing, add the target div to collapsingFilters to ignore additional clicks until animation completes
+      collapsingFilters.add($filterElement[0]);
+    }
+    const $filterInput = $("#activeFilters");
+    btn.classList.toggle("btn-outline-primary");
+    btn.classList.toggle("btn-primary");
+    if (!init) {
+      if (filters.has(thisFilter))
+        filters.delete(thisFilter);
+      else
+        filters.add(thisFilter);
+    }
+    $("#searchBtn").prop("disabled", filters.size === 0);
+    $("#andOrDiv").collapse(filters.size > 1 ? "show" : "hide");
+    $filterInput.val(JSON.stringify(Array.from(filters)));
   }
-  $("#searchBtn").prop("disabled", filters.size === 0);
-  $("#andOrDiv").collapse(filters.size > 1 ? "show" : "hide");
-  $filterInput.val(JSON.stringify(Array.from(filters)));
 }
 
 function toggleAndOr() {
-  let $btn = $("#andOrBtn");
-  let $hidden = $("#andOrHidden");
+  const $btn = $("#andOrBtn");
+  const $hidden = $("#andOrHidden");
   let text, val;
   if ($hidden.val() === "and") {
     text = "Results match ANY filter";
