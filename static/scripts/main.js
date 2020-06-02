c2639fc2680b52ef01604cfe370b51dd250c2b8a
Zac Hayes <zrev2220@gmail.com>
2020-06-01 22:32:28 -0400

    Display multiple alerts in same space, stacked/staggered slightly

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,11 @@
-// put some code here
+$(() => {
+  // stagger multiple alerts slightly
+  let margin = 0;
+  $("#message-alerts .alert").each((i, e) => {
+    $(e).css("margin-top", margin);
+    // stop increasing margin after 5
+    if (i < 5) {
+      margin += 5;
+    }
+  });
+});
