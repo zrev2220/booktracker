a41d77bd4ffc7ac108873eab5e6604459799fece
Zac Hayes <zrev2220@gmail.com>
2020-06-25 21:54:40 -0400

    Remember form data on login failure

--- booktracker/views.py
+++ booktracker/views.py
@@ -3,3 +3,12 @@ from django.contrib.auth.views import LoginView
 
 class MyLoginView(LoginView):
     template_name = "login.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.method == "POST":
+            form = self.get_form()
+            if not form.is_valid():
+                context["username"] = form.cleaned_data["username"]
+                context["password"] = form.cleaned_data["password"]
+        return context
