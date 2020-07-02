7293318a82f0575f262064fe6d0e3283ff2fd49f
Zac Hayes <zrev2220@gmail.com>
2020-07-01 22:56:16 -0400

    Add app version to navbar & login page
    Closes #23.

--- booktracker/settings.py
+++ booktracker/settings.py
@@ -14,6 +14,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()  # Load environment vars from .env file (if present)
 
+APP_VERSION = "1.0.0"
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booktracker',
     'books.apps.BooksConfig',
     'people.apps.PeopleConfig',
 ]
