bff0db38643e5aadb86fd6e41b23e323fb94c948
Zac Hayes <zrev2220@gmail.com>
2020-12-25 17:19:04 -0500

    Test CI/CD detecting lint & format issues

--- booktracker/views.py
+++ booktracker/views.py
@@ -5,10 +5,11 @@ class MyLoginView(LoginView):
     template_name = "login.html"
 
     def get_context_data(self, **kwargs):
+        x=0
         context = super().get_context_data(**kwargs)
         if self.request.method == "POST":
             form = self.get_form()
             if not form.is_valid():
-                context["username"] = form.cleaned_data["username"]
+                context["username"] =   form.   cleaned_data["username"]
                 context["password"] = form.cleaned_data["password"]
         return context
