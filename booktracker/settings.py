zrev2220/booktracker

commit 335ca221a489e493ea6bde31cc453835e8708040

Create React frontend app

booktracker/settings.py
@@ -45,10 +45,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cra_helper",
     "django.contrib.staticfiles",
-    "booktracker",
-    "books.apps.BooksConfig",
-    "people.apps.PeopleConfig",
+    "frontend",
 ]
 
 MIDDLEWARE = [
@@ -74,6 +73,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "cra_helper.context_processors.static"
             ],
         },
     },
@@ -138,3 +138,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_prod")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_FINDERS = [
+    # Required for CRAManifestFinder below to work
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    # A finder to pull in asset-manifest.json
+    "cra_helper.finders.CRAManifestFinder",
+]
+
+# Django CRA Helper settings
+CRA_APP_NAME = "frontend"
+# CRA_HOST = "0.0.0.0"  # hostname of CRA liveserver
+# CRA_PORT = 3000  # port of CRA liveserver

.gitignore
@@ -1,10 +1,33 @@
-venv/
+# editors
 .idea/
+
+# dependencies
+venv/
+node_modules/
+.pnp/
+.pnp.js
+
+# testing
+coverage/
+
+# production
+build/
+
+# misc
+.DS_Store
+*.env
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
 __pycache__/
-static_prod/
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
 
 *.sqlite3
-*.env
 
+static_prod/
 stuff/
-

booktracker/urls.py
@@ -13,23 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path, include
-
-from booktracker.views import MyLoginView
+from django.urls import path, include, re_path
+from cra_helper.views import proxy_cra_requests
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("books.urls")),
-    path("login/", MyLoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    # path('checkout/<id>', ),
-    # path('people/', ),
-    # path('people/add', ),
+    path("", include("frontend.urls")),
 ]
 
 handler400 = "booktracker.errorviews.error400"
 handler403 = "booktracker.errorviews.error403"
 handler404 = "booktracker.errorviews.error404"
 handler500 = "booktracker.errorviews.error500"
+
+# add a reverse-proxy view to help React in the Django view talk to Create-React-App
+if settings.DEBUG:
+    proxy_urls = [
+        re_path(r'^__webpack_dev_server__/(?P<path>.*)$', proxy_cra_requests),
+        re_path(r'^(?P<path>.+\.hot-update\.(js|json|js\.map))$', proxy_cra_requests),
+    ]
+    urlpatterns.extend(proxy_urls)

frontend/public/index.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <!--
+      manifest.json provides metadata used when your web app is installed on a
+      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+    -->
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <!--
+      Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder can be referenced from the HTML.
+
+      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+      work correctly both with client-side routing and a non-root public URL.
+      Learn how to configure a non-root public URL by running `npm run build`.
+    -->
+    <title>React App</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <!--
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+    -->
+  </body>
+</html>

frontend/src/App.css
@@ -1,0 +1,38 @@
+.App {
+  text-align: center;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}

commit 34a90962baf69e3d3e83681f8b56edf82e8fe696

Fix tests
Removing app registrations in 335ca221 broke tests. Adding them back for
now to make tests work. (We'll likely remove/change as React+DRF goes
onward.)

booktracker/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     "cra_helper",
     "django.contrib.staticfiles",
     "frontend",
+    # TODO ↓ remove/revisit these app registrations if we don't need them
+    "books.apps.BooksConfig",
+    "people.apps.PeopleConfig",
 ]
 
 MIDDLEWARE = [